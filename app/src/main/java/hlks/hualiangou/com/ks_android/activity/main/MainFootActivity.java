67b4232aee77906ecc387f85316ac5d6a0f1d5dd
axi0712 <1255645340@qq.com>
2018-02-01 14:03:31 +0800

    增加购物车有积分商品时的业务逻辑

--- app/src/main/java/hlks/hualiangou/com/ks_android/activity/main/MainFootActivity.java
+++ app/src/main/java/hlks/hualiangou/com/ks_android/activity/main/MainFootActivity.java
@@ -1,21 +1,26 @@
 package hlks.hualiangou.com.ks_android.activity.main;
 
-import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
-import android.widget.TableLayout;
 import android.widget.TextView;
 
 import com.tsy.sdk.myokhttp.response.GsonResponseHandler;
-import com.tsy.sdk.myokhttp.response.RawResponseHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import hlks.hualiangou.com.ks_android.App;
 import hlks.hualiangou.com.ks_android.R;
 import hlks.hualiangou.com.ks_android.base.BaseActivity;
+import hlks.hualiangou.com.ks_android.base.BaseFragment;
 import hlks.hualiangou.com.ks_android.bean.MyFootBean;
+import hlks.hualiangou.com.ks_android.fragment.FootFragment;
 import hlks.hualiangou.com.ks_android.modle.url.UrlUtilds;
 import hlks.hualiangou.com.ks_android.utils.UserUtils;
 
@@ -26,23 +31,29 @@ public class MainFootActivity extends BaseActivity implements View.OnClickListen
      * 编辑
      */
     private TextView mMainFootEdit;
-    private TableLayout mMainFootTab;
+    private TabLayout mMainFootTab;
     private ViewPager mMainFootPager;
+    private List<String> listTitle;
+    private List<BaseFragment> listFragment;
     /**
      * 我的足迹的实体类
      */
     private MyFootBean myFootBean;
+
     @Override
     public int getLayoutId() {
         return R.layout.activity_main_foot;
     }
+
     @Override
     public void initView() {
+        listTitle = new ArrayList<>();
+        listFragment = new ArrayList<>();
         mGoBack = (RelativeLayout) findViewById(R.id.go_back);
         mGoBack.setOnClickListener(this);
         mMainFootEdit = (TextView) findViewById(R.id.main_foot_edit);
         mMainFootEdit.setOnClickListener(this);
-        mMainFootTab = (TableLayout) findViewById(R.id.main_foot_tab);
+        mMainFootTab = (TabLayout) findViewById(R.id.main_foot_tab);
         mMainFootPager = (ViewPager) findViewById(R.id.main_foot_pager);
     }
 
@@ -68,6 +79,7 @@ public class MainFootActivity extends BaseActivity implements View.OnClickListen
                 break;
         }
     }
+
     private void initOkhttp() {
         App.myOkHttp
                 .postParams()
@@ -80,7 +92,18 @@ public class MainFootActivity extends BaseActivity implements View.OnClickListen
                 .enqueue(new GsonResponseHandler<MyFootBean>() {
                     @Override
                     public void onSuccess(int statusCode, MyFootBean response) {
-
+                        List<MyFootBean.MsgBean.TimeBean> timeList = response.getMsg().getTime();
+                        for (MyFootBean.MsgBean.TimeBean timeBean : timeList) {
+                            listTitle.add(timeBean.getKey());
+                            Log.e("TAG","title==》"+timeBean.getKey());
+                        }
+                        for (int i = 0; i < listTitle.size(); i++) {
+                            FootFragment footFragment = new FootFragment();
+                            listFragment.add(footFragment);
+                        }
+                        MyAdapter myAdapter = new MyAdapter(getSupportFragmentManager(), listTitle, listFragment);
+                        mMainFootPager.setAdapter(myAdapter);
+                        mMainFootTab.setupWithViewPager(mMainFootPager);
                     }
 
                     @Override
@@ -88,5 +111,34 @@ public class MainFootActivity extends BaseActivity implements View.OnClickListen
 
                     }
                 });
+    }
+
+    class MyAdapter extends FragmentPagerAdapter {
+
+        private List<String> listTitle;
+
+        private List<BaseFragment> listFragment;
+
+        public MyAdapter(FragmentManager fm, List<String> listTitle, List<BaseFragment> listFragment) {
+            super(fm);
+            this.listTitle = listTitle;
+            this.listFragment = listFragment;
+        }
+
+
+        @Override
+        public Fragment getItem(int i) {
+            return listFragment.get(i);
+        }
+
+        @Override
+        public int getCount() {
+            return listFragment.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return listTitle.get(position);
+        }
     }
 }
---
+++ app/src/main/java/hlks/hualiangou/com/ks_android/fragment/FootFragment.java
@@ -0,0 +1,40 @@
+package hlks.hualiangou.com.ks_android.fragment;
+
+import android.view.View;
+
+import hlks.hualiangou.com.ks_android.R;
+import hlks.hualiangou.com.ks_android.base.BaseFragment;
+
+/**
+ * /**
+ * 项目名称: 药到家
+ * 类描述:
+ * 创建人: XI
+ * 创建时间: 2018/2/1 0001 13:22
+ * 修改人:
+ * 修改内容:
+ * 修改时间:
+ */
+
+
+public class FootFragment extends BaseFragment {
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_foot;
+    }
+
+    @Override
+    public void initView(View view) {
+
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+
+    @Override
+    public void setListener() {
+
+    }
+}
